5433f1834cd9cb023eabf7061c923ed8ab3bbb96
Fiore Giuseppe <fioreg@corp.dom>
2018-06-13 10:31:05 +0200

    fix

--- webhook_routes.js
+++ webhook_routes.js
@@ -8,6 +8,31 @@ module.exports = function(webserver, controller) {
 
         var bot = controller.spawn({});
 
+        //
+        // Webex Teams Utilities
+        //
+
+        // Utility to add mentions if Bot is in a 'Group' space
+        bot.appendMention = function(message, command) {
+
+            // if the message is a raw message (from a post message callback such as bot.say())
+            if (message.roomType && (message.roomType == "group")) {
+                var botName = bot.botkit.identity.displayName;
+                return "`@" + botName + " " + command + "`";
+            }
+
+            // if the message is a Botkit message
+            if (message.raw_message && (message.raw_message.data.roomType == "group")) {
+                var botName = bot.botkit.identity.displayName;
+                return "`@" + botName + " " + command + "`";
+            }
+
+            return "`" + command + "`";
+        }
+
+        // [COMPAT] Adding this function to ease interoperability with the skills part of the Botkit samples project
+        bot.enrichCommand = bot.appendMention;
+
         // Now, pass the webhook into be processed
         controller.handleWebhookPayload(req, res, bot);
     });
