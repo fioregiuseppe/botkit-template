a915023bc4631d17845e452811ba7ce71de4a3c6
Fiore Giuseppe <fioreg@corp.dom>
2018-06-13 11:24:05 +0200

    fix

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var controller = require('./bot/webex/controller.js')();
+var webex_controller = require('./bot/webex/controller.js')();
+var slack_controller = require('./bot/webex/controller.js')();
 // Set up an Express-powered webserver to expose oauth and webhook endpoints
 // We are passing the controller object into our express server module
 // so we can extend it and process incoming message payloads 
-var webserver = require('./webserver.js')(controller);
-require('./bot/webex/init.js')(controller, webserver);
-require('./bot/slack/init.js')(controller, webserver);
+var webserver = require('./webserver.js')();
+require('./bot/webex/init.js')(webex_controller, webserver);
+require('./bot/slack/init.js')(slack_controller, webserver);
